21a224c3ea39f49274eb4aca2ee5e6c09aa08347
Adam Urbina <adamurbina@gmail.com>
2019-01-24 12:10:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ const returnFirstTwoDrivers  = function (drivers){
 const returnLastTwoDrivers  = function (drivers){
   return drivers.slice(-2);
 }
+
+const selectingDrivers = function (drivers) {
+  array = [];
+  array.push(returnFirstTwoDrivers(drivers));
+  array.push(returnLastTwoDrivers(drivers));
+  return array; 
+}
